b1b8e5b660a5da0ebbaa02722395f5b8d34156e6
Pranay Gole <pranay123@gmail.com>
2025-11-09 23:17:17 +0530

    Fixed Render port binding issue

--- app.py
+++ app.py
@@ -288,7 +288,9 @@ def flappy():
 
 
 # -----------------------
-# Run App
+# Run App (Render-ready)
 # -----------------------
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
